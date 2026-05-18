3915fd557d39a0202c85c39641de0976e4892c1c
Usuario <andressassiqueirra@gmail.com>
2026-05-18 14:13:03 -0400

    Um monte de coisa

---
+++ android/app/src/main/java/checklist/setup/MainActivity.java
@@ -0,0 +1,5 @@
+package checklist.setup;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'checklist.setup',
+  appName: 'Projeto_CheckList_Setup_Qualidade',
+  webDir: 'dist'
+};
+
+export default config;
